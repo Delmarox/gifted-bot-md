ba7322f67a0fcd3050d4ba3c927b2a2d088808a5
Delmarox <165329046+Delmarox@users.noreply.github.com>
2024-07-10 17:06:20 +0200

    Update config.js
    Signed-off-by: Delmarox <165329046+Delmarox@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
-
+ 
 
 //═══════[Required Variables]════════\\
 global.audio= "" ;  
